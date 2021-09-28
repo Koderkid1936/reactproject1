0bc8418db10f789ef37600d7cb5fe2bc1b8641a2
Koderkid1936 <evers.be2013@outlook.com>
2021-09-28 10:44:45 +0100

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
     <div className='hero-container' id="home">
       <video src='/videos/video-2.mp4' autoPlay loop muted />
       <h1>Portfolio Page</h1>
-      <p>Hello everyone</p>
+      <p>Hello everyone!</p>
       <div className='hero-btns'>
         <Button
           className='btns'
